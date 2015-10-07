6c5361fd86b9072d37fdc357d39285887151bbc1
giuseppe de santis <giusdesan@gmail.com>
2015-10-07 18:15:02 +0100

    minor refactor

--- lib/journey.rb
+++ lib/journey.rb
@@ -16,11 +16,21 @@ class Journey
   end
 
   def calculate_fare(start, finish)
-    @stations[start.to_sym]=="B" || @stations[finish.to_sym]=="B" ? 300 : 250
+    zone_b?(start, finish) ? 300 : 250
+  end
+
+  def cap_checker(start, finish, card)
+    card.increase_cap if zone_b?(start, finish)
   end
 
   def charge_to(start, finish, card)
     fare = calculate_fare(start, finish)
     card.charge(fare)
+  end
+
+  private
+
+  def zone_b?(start, finish)
+    @stations[start.to_sym]=="B" || @stations[finish.to_sym]=="B"
   end
 end
